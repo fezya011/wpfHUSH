8ee0726e6aa16e98e4b17616a08476daf5c460c5
Wakeupparalyze <envy--e@mail.ru>
2025-04-14 10:25:30 +1000

    Добавьте файлы проекта.

---
+++ Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfHUSH.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+        public byte[] Photo { get; set; }
+        public int LoginPasswordId { get; set; }
+        public LoginPassword LoginPassword { get; set; }
+
+    }
+
+}
+
+       
